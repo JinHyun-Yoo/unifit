80e5568375f351ae124494f9beef2a073210c8df
JinHyun-Yoo <84774425+JinHyun-Yoo@users.noreply.github.com>
2026-05-20 15:31:14 +0900

    student.h

--- student.h
+++ student.h
@@ -1,33 +1,43 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
-typedef struct Student {
-    int id;
-    char name[30];
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-    int korean;
-    int english;
-    int math;
-    int science;
+    typedef struct Student {
+        int id;
+        char name[30];
 
-    double average;
-    double grade;
+        int korean;
+        int english;
+        int math;
+        int science;
 
-    struct Student* left;
-    struct Student* right;
-} Student;
+        double average;
+        double grade;
 
-double calculateAverage(int korean, int english, int math, int science);
-double calculateGrade(double average);
+        struct Student* left;
+        struct Student* right;
+    } Student;
 
-Student* createStudent(int id, char name[], int korean, int english, int math, int science);
-Student* insertStudent(Student* root, Student* newStudent);
-Student* searchStudent(Student* root, int id);
-Student* deleteStudent(Student* root, int id);
+    double calculateAverage(int korean, int english, int math, int science);
+    double calculateGrade(double average);
 
-void updateStudentInfo(Student* root, int id);
+    Student* createStudent(int id, char name[], int korean, int english, int math, int science);
+    Student* insertStudent(Student* root, Student* newStudent);
+    Student* searchStudent(Student* root, int id);
+    Student* deleteStudent(Student* root, int id);
 
-void printStudent(Student* student);
-void freeTree(Student* root);
+    Student* is_login(void);          // 학생 정보 입력 함수
+    void is_remake(Student* student); // 학생 정보 최신화 함수
+    void is_weak(Student* student);   // 약점 탐색 함수
+
+    void printStudent(Student* student);
+    void freeTree(Student* root);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
