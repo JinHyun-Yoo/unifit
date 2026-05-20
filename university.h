b7c7223c8525eba2d47faa39a648e4e3df24aeae
JinHyun-Yoo <84774425+JinHyun-Yoo@users.noreply.github.com>
2026-05-20 15:31:47 +0900

    university.h

--- university.h
+++ university.h
@@ -3,12 +3,23 @@
 
 #include "student.h"
 
-typedef struct University {
-    char name[50];
-    char major[50];
-    double cutline;
-} University;
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-void compareUniversity(Student* student);
+    typedef struct University {
+        char name[50];
+        char major[50];
+        char type[30];
+        double cutline;
+    } University;
+
+    void is_verse(Student* student); // 대학 입시 예상 및 지원
+    void is_review(void);           // 모의 면접 & 대학 후기
+    void is_date(void);             // 대학 및 입시 관련 소식
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
